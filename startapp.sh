313001c4c08281c49b87a4dcb7ddbcc9087a19a7
Charles de Freitas <me@chasbob.dev>
2020-10-23 07:57:54 +0100

    WIP no sound

--- startapp.sh
+++ startapp.sh
@@ -1,2 +1,25 @@
 #!/bin/sh
-exec /usr/bin/vba -F
+
+set -eu
+
+# docker run parameters for pulseaudio
+# --group-add "$(getent group audio | cut -d: -f3)"
+# -e "PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native"
+# -v "${XDG_RUNTIME_DIR}/pulse/native/:${XDG_RUNTIME_DIR}/pulse/native"
+# -v "${HOME}/.config/pulse/cookie:/tmp/pulseaudio_cookie"
+PULSEAUDIO_COOKIE_TMP_MOUNT_PATH=/data/pulseaudio_cookie
+VBAM_CONFIG_TMP_MOUNT_PATH=/data/vbam_conf
+
+# USER_NAME=user
+#
+# useradd -u "${HOST_UID}" -o -m "${USER_NAME}"
+# groupmod -g "${HOST_GID}" "${USER_NAME}"
+#
+# chown -R "${USER_NAME}:${USER_NAME}" /vbam
+#
+# gosu "${USER_NAME}" mkdir -p "/home/${USER_NAME}/.config/pulse"
+# cp "${PULSEAUDIO_COOKIE_TMP_MOUNT_PATH}" "/home/${USER_NAME}/.config/pulse/cookie"
+# cp -r "${VBAM_CONFIG_TMP_MOUNT_PATH}" "/home/${USER_NAME}/.config/visualboyadvance-m"
+
+# gosu "${USER_NAME}" visualboyadvance-m "$@"
+exec visualboyadvance-m
